c815ad2673bdc7fbce03fe5a7a6a0518c4037248
Vincenzo M <vincenzomaiorana@gmail.com>
2023-02-28 15:20:23 +0100

    easteregg

--- src/componets/Home/Home.tsx
+++ src/componets/Home/Home.tsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
 export const Home = () => {
   return (
-    <>
+    <div className="text-center mb-5">
+      <img
+        className="mb-3"
+        src="https://img.freepik.com/premium-photo/question-will-internet-go-away-banner-men-s-hand-with-blurred-background-link-crash-crisis-client-ai-tech-speed-risk-free-globe-cable-line-cyber-device-wifi-net_75939-893.jpg?w=360"
+        alt="Roba carina"
+      />
+      <h2 className="text-center">Hai già finito l'internet!</h2>
       <Link to="/profile/me">
-        <h2>home</h2>
+        <h3 className="text-center">Visita il tuo profilo</h3>
       </Link>
-    </>
+      <h6 className="text-center mb-5">Se vuoi, oppure rimani qua a fissare il nulla per un altro po!</h6>
+    </div>
   );
 };
