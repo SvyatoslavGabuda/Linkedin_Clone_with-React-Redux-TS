aa285ddf2b553a129f9644bf7445011b717983e2
SvyatoslavGabuda <sgabuda@gmail.com>
2023-02-28 10:23:26 +0100

    fetc in home

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,37 @@ import { Container } from "react-bootstrap";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 //import { Counter } from "./features/counter/Counter";
 import "./App.scss";
+import { useAppDispatch } from "./app/hooks";
+import { ADD_TO_MYPROFILE } from "./app/reducers/allProfileReduce";
 import { Chat } from "./componets/Chat/Chat";
 import MyFooter from "./componets/Footer/MyFooter";
 import { Home } from "./componets/Home/Home";
 import MyNav from "./componets/NavBar/MyNav";
 import Profile from "./componets/Profile/Profile";
+import { useEffect } from "react";
+
+const url = "https://striveschool-api.herokuapp.com/api/profile/";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const myProfileFetch = async () => {
+    try {
+      const response = await fetch(url + "me", {
+        headers: {
+          Authorization: process.env.REACT_APP_BEARER || "nonandra",
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log("myprofle", data);
+        // setMyProfile(data);
+        dispatch({ type: ADD_TO_MYPROFILE, payload: data });
+      }
+    } catch (error) {}
+  };
+  useEffect(() => {
+    myProfileFetch();
+  }, []);
   return (
     <>
       <BrowserRouter>
