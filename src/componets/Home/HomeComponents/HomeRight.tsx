0adf252716cc8ef1e198a7f8092e84496a4a9bb9
Vincenzo M <vincenzomaiorana@gmail.com>
2023-03-01 10:13:54 +0100

    modev home sidebars, created right sidebar

--- src/componets/Home/HomeComponents/HomeRight.tsx
+++ src/componets/Home/HomeComponents/HomeRight.tsx
@@ -1,3 +1,37 @@
+import { Col } from "react-bootstrap";
+
 export const HomeRight = () => {
-  return <></>;
+  return (
+    <>
+      <Col xl={3} className="d-none d-xl-block">
+        <div className="HomeRightPartContainer border border-1 rounded rounded-3">
+          <div className="HomeRightPartHeader">
+            <h5>LinkedIn Notizie</h5>
+          </div>
+          <ul>
+            <li>
+              <h6>Nuova proproga per lo smart working</h6>
+              <p>1 giorno fa | 590 lettori</p>
+            </li>
+            <li>
+              <h6>Nuova proproga per lo smart working</h6>
+              <p>1 giorno fa | 590 lettori</p>
+            </li>
+            <li>
+              <h6>Nuova proproga per lo smart working</h6>
+              <p>1 giorno fa | 590 lettori</p>
+            </li>
+            <li>
+              <h6>Nuova proproga per lo smart working</h6>
+              <p>1 giorno fa | 590 lettori</p>
+            </li>
+            <li>
+              <h6>Nuova proproga per lo smart working</h6>
+              <p>1 giorno fa | 590 lettori</p>
+            </li>
+          </ul>
+        </div>
+      </Col>
+    </>
+  );
 };
