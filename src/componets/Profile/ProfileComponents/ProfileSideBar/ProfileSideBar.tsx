178f29e7d22181576bc57fca61c63a50bf08f2de
Alessandro Ontani <casualotn@gmail.com>
2023-03-02 22:32:43 +0100

    working on the profiles randomizer for the sidebar

--- src/componets/Profile/ProfileComponents/ProfileSideBar/ProfileSideBar.tsx
+++ src/componets/Profile/ProfileComponents/ProfileSideBar/ProfileSideBar.tsx
@@ -4,14 +4,30 @@ import { UpperSectionComponent } from "./SideBarComponents/UpperSectionComponent
 import { OtherBusinessComponent } from "./SideBarComponents/OtherBusinessComponents";
 import { OtherPersonsComponent } from "./SideBarComponents/OtherPersonsComponent";
 import { GroupsInterestsComponent } from "./SideBarComponents/GroupsInterestsComponent";
+import { useAppSelector } from "../../../../app/hooks";
+import { Iprofile } from "../../Profile";
 
 export const ProfileSideBar = () => {
+  const fetchedProfiles = useAppSelector((state) => state.profile.allProfile);
+  const myProfiles = [...fetchedProfiles];
+  console.log("based", myProfiles);
+  const randomizeIt = (profiles: Iprofile[]) => {
+    for (let i = profiles.length - 1; i > 0; i--) {
+      let randomIndex = Math.floor(Math.random() * (i + 1));
+      [profiles[i], profiles[randomIndex]] = [profiles[randomIndex], profiles[i]];
+    }
+    console.log("random", profiles);
+  };
+
   return (
-    <Col xs={12} md={5} lg={4}>
-      <UpperSectionComponent />
-      <OtherBusinessComponent />
-      <OtherPersonsComponent />
-      <GroupsInterestsComponent />
-    </Col>
+    <>
+      <Col xs={12} md={5} lg={4}>
+        <UpperSectionComponent />
+        <OtherBusinessComponent />
+        <OtherPersonsComponent />
+        <GroupsInterestsComponent />
+      </Col>
+      {fetchedProfiles.length > 0 && randomizeIt(myProfiles)}
+    </>
   );
 };
