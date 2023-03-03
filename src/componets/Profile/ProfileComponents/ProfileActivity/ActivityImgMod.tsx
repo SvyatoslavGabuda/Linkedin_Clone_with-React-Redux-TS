1e98f7b8fdfcfad72d659e7cedddb2778aee94a0
Alessandro Ontani <casualotn@gmail.com>
2023-03-03 16:04:43 +0100

    fixed input type file css in Profile activity post modal

--- src/componets/Profile/ProfileComponents/ProfileActivity/ActivityImgMod.tsx
+++ src/componets/Profile/ProfileComponents/ProfileActivity/ActivityImgMod.tsx
@@ -3,6 +3,7 @@ import { FormEvent, ChangeEvent, useState } from "react";
 import { Iposts, postsFetc } from "../../../../app/reducers/postsSlice";
 import { Add } from "../../../../app/reducers/slicerForUpDate";
 import { useAppDispatch } from "../../../../app/hooks";
+import { BsImage } from "react-icons/bs";
 
 interface ProfileImageMod {
   show: boolean;
@@ -16,16 +17,13 @@ export const ActivityImgMod = (props: ProfileImageMod) => {
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     try {
-      const response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/posts/" + props.lastpost[0]._id,
-        {
-          method: "POST",
-          body: image,
-          headers: {
-            Authorization: process.env.REACT_APP_BEARER || "nonandra",
-          },
-        }
-      );
+      const response = await fetch("https://striveschool-api.herokuapp.com/api/posts/" + props.lastpost[0]._id, {
+        method: "POST",
+        body: image,
+        headers: {
+          Authorization: process.env.REACT_APP_BEARER || "nonandra",
+        },
+      });
       if (response.ok) {
         console.log("immagine Caricata");
       } else {
@@ -58,20 +56,19 @@ export const ActivityImgMod = (props: ProfileImageMod) => {
           handleSubmit(e);
         }}
       >
-        <Modal.Body>
+        <Modal.Body className="d-flex flex-column align-items-center pb-0">
           {props.lastpost[0]?.image && (
             <div>
               <h6 className="mb-3">Immagine attuale: </h6>
-              <img
-                src={props.lastpost[0].image}
-                style={{ width: "100%" }}
-                alt="Active Post"
-                className="mb-3"
-              />
+              <img src={props.lastpost[0].image} style={{ width: "100%" }} alt="Active Post" className="mb-3" />
             </div>
           )}
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control type="file" placeholder="" onChange={handleLoadFile} />
+          <Form.Group className="mb-3 text-center" controlId="formBasicEmail">
+            <label className="inputLabelExp d-flex align-items-center justify-content-center py-2 px-5">
+              <span>Carica</span>
+              <BsImage className="mx-2" />
+              <Form.Control type="file" placeholder="" onChange={handleLoadFile} style={{ display: "none" }} />
+            </label>
           </Form.Group>
         </Modal.Body>
         <Modal.Footer>
