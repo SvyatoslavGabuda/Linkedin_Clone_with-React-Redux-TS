84ac9ed2fa44530e6f26a1662070568825aed8df
Vincenzo M <vincenzomaiorana@gmail.com>
2023-03-03 12:37:42 +0100

    metadata changed like original linkedin

--- src/componets/Home/Home.tsx
+++ src/componets/Home/Home.tsx
@@ -2,8 +2,10 @@ import { HomeLeftComponent } from "./HomeComponents/HomeLeftComponent";
 import { HomeMid } from "./HomeComponents/HomeMid";
 import { Row } from "react-bootstrap";
 import { HomeRight } from "./HomeComponents/HomeRight";
+import useDocumentTitle from "../../app/useDocumentTitle";
 
 export const Home = () => {
+  useDocumentTitle(`Feed | LinkedIn`);
   return (
     <>
       <Row>
