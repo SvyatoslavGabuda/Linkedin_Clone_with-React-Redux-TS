119fb08568359065e2e64203028377c4d42022d5
Vincenzo M <vincenzomaiorana@gmail.com>
2023-03-06 11:18:05 +0100

    added title in jobs page

--- src/componets/Jobs/Jobs.tsx
+++ src/componets/Jobs/Jobs.tsx
@@ -2,11 +2,31 @@ import "./jobs.scss";
 import { useEffect } from "react";
 import { useAppDispatch } from "../../app/hooks";
 import { jobsFetch } from "../../app/reducers/jobsSlice";
+import { Col, Row } from "react-bootstrap";
+import { HomeFooter } from "../Home/HomeComponents/HomeFooter";
+import useDocumentTitle from "../../app/useDocumentTitle";
 
 export const Jobs = () => {
   const dispatch = useAppDispatch();
+  const limit = 20;
+
+  useDocumentTitle(`Offerte di lavoro | LinkedIn`);
+
   useEffect(() => {
-    dispatch(jobsFetch(20));
-  });
-  return <>Jobs</>;
+    dispatch(jobsFetch(limit));
+  }, [limit]);
+
+  return (
+    <>
+      <Row>
+        <Col xs={12} md={4} xl={3} className="px-0 px-md-3">
+          Prima
+        </Col>
+        <Col>Seconda</Col>
+        <Col xl={3} className="d-none d-xl-block">
+          <HomeFooter />
+        </Col>
+      </Row>
+    </>
+  );
 };
