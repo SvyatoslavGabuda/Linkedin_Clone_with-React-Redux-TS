e6c21f336aba2c7a42bda7f7fa8b18404fd7d349
Vincenzo M <vincenzomaiorana@gmail.com>
2023-03-06 12:57:09 +0100

    created job component

---
+++ src/componets/Jobs/JobComponent.tsx
@@ -0,0 +1,17 @@
+export const JobComponent = () => {
+  return (
+    <div className="jobcard d-flex">
+      <div>
+        <img src="https://placekitten.com/200" alt="Azienda" />
+      </div>
+      <div>
+        <h6>Ingegnere informatico</h6>
+        <p>Idea srl</p>
+        <p>Porto Empedocle, Sicilia, Italia (Ibrido)</p>
+        <p>Selezione attiva</p>
+        <p>2 settimane fa - 23 candidati</p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
