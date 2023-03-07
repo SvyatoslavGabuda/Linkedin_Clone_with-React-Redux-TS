2ef712181bfba0dba7db0b1931bc381fa07be068
SvyatoslavGabuda <sgabuda@gmail.com>
2023-03-07 22:08:20 +0100

    player

--- src/snakegame/GamePage.tsx
+++ src/snakegame/GamePage.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
+import { BsPauseCircleFill, BsPlayCircleFill } from "react-icons/bs";
 import Game from "./game/Game";
-import { GameContainer } from "./gamePage.style";
+
+import { GameContainer, PlayerBtn } from "./gamePage.style";
+import track from "./songs/songs.mp3";
 export const GamePage = () => {
+  const [player, setPlayer] = useState(new Audio(track));
+  const [playPause, setPlayPause] = useState(false);
+
+  const handlePlay = () => {
+    if (playPause) {
+      player.pause();
+      setPlayPause(false);
+    } else {
+      player.play();
+      setPlayPause(true);
+    }
+  };
+  useEffect(() => {
+    player.play();
+    setPlayPause(true);
+  }, []);
   return (
     <>
       <GameContainer style={{ backgroundImage: "url('assets/image/sfond2.jpg')" }}>
+        <PlayerBtn onClick={handlePlay}>
+          {playPause ? <BsPauseCircleFill /> : <BsPlayCircleFill />}
+        </PlayerBtn>
         <Game />
       </GameContainer>
     </>
